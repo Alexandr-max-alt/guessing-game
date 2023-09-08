f94468500ea7069dcfc19ebd0905e445822140c4
Alexandr-max-alt <doberman8636@yandex.ru>
2023-09-08 18:38:02 +0300

    create solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,34 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor(min,max) {
+        this.values = [];
+    }
 
     setRange(min, max) {
-
+        while (min <= max) {
+            this.values.push(min)
+            min+=1
+        }
+        return this.values
     }
 
     guess() {
-
+        let right = this.values.length - 1
+        let left = 0
+        let mid = Math.round((right - left) / 2) + left;
+        return this.values[mid]
     }
 
     lower() {
-
+        const res = this.values[Math.floor(this.values.length / 2 - 1)]
+        this.values = this.values.slice(0, this.values.indexOf(res+2))
+        return this.values
     }
 
     greater() {
-
+        const res = this.values[Math.floor(this.values.length / 2 - 1)]
+        this.values = this.values.slice(this.values.indexOf(res + 1), this.values[this.values.length])
+        return this.values
     }
 }
 
